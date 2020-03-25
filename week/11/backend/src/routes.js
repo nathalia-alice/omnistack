1c05218aa0c8a31e287cc89a31e857bf8c205e86
nathalia-alice <nathalia.alice.silv@gmail.com>
2020-03-24 23:03:29 -0300

    Configurando rotas

---
+++ week/11/backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.post('/users', (request, response) => {
+    const params = request.body;
+    console.log(params)
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Nathalia Alice'
+    });
+})
+
+module.exports = routes;
