037b61d66d5c32a26a1c96d503571d55d55e7dd1
nathalia-alice <nathalia.alice.silv@gmail.com>
2020-03-26 22:38:28 -0300

    Enviando e buscando Ongs

--- week/11/backend/src/routes.js
+++ week/11/backend/src/routes.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    const params = request.body;
-    console.log(params)
-    return response.json({
-        evento: 'Semana Omnistack 11.0',
-        aluno: 'Nathalia Alice'
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+
+    return response.json(ongs);
+})
+
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     });
+
+    return response.json({ id });
 })
 
 module.exports = routes;
